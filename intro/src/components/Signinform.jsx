d8f496d3bf7c003ff6397e55c6216d277ed30c4e
Oluwasuyi-Timilehin <oluwasuyitimilehin@gmail.com>
2025-05-23 02:24:53 +0100

    Refactor Signinform component for improved readability and formatting

--- intro/src/components/Signinform.jsx
+++ intro/src/components/Signinform.jsx
@@ -1,19 +1,28 @@
-
-
 const Signinform = () => {
   return (
     <>
-      <form action="" className="h-screen flex items-center justify-center lg:w-1/2">
+      <form
+        action=""
+        className="h-screen flex items-center justify-center lg:w-1/2"
+      >
         <div className="bg-white shadow-lg rounded-lg p-6 border border-zinc-100 flex flex-col w-96 space-y-5">
           <div className="flex flex-col space-y-2">
             <label className="text-md md:text-lg text-zinc-800">Username</label>
-            <input type="text" className="py-1.5 px-3 border border-gray-300 rounded focus:outline-none" />
+            <input
+              type="text"
+              className="py-1.5 px-3 border border-gray-300 rounded focus:outline-none"
+            />
           </div>
           <div className="flex flex-col space-y-2">
             <label className="text-md md:text-lg text-zinc-800">Password</label>
-            <input type="text" className="py-1.5 px-3 border border-gray-300 rounded focus:outline-none" />
+            <input
+              type="text"
+              className="py-1.5 px-3 border border-gray-300 rounded focus:outline-none"
+            />
           </div>
-          <button className="bg-emerald-600 text-white rounded py-2 font-semibold">Log In</button>
+          <button className="bg-emerald-600 text-white rounded py-2 font-semibold">
+            Log In
+          </button>
         </div>
       </form>
     </>
