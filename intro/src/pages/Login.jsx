607af9fd3e4cd68b8edae426ebd157acadf11724
Oluwasuyi-Timilehin <oluwasuyitimilehin@gmail.com>
2025-05-23 02:10:10 +0100

    Add Signinform component and integrate it into the Login page layout

--- intro/src/pages/Login.jsx
+++ intro/src/pages/Login.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import Signinform from "../components/Signinform";
 
 const Login = () => {
   return (
     <>
-      <h1 className="text-5xl">Login</h1>
+      <div className="flex items-center">
+        <div className="hidden md:flex p-3 h-screen bg-blue-900 w-1/2">
+          <h1 className="text-5xl">Login</h1>
+        </div>
+        <Signinform />
+      </div>
     </>
   );
 };
