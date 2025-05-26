971a2ea070db46d6adfee4e77ff0667bce8ab58b
Oluwasuyi-Timilehin <oluwasuyitimilehin@gmail.com>
2025-05-26 23:35:36 +0100

    changed the background

--- intro/src/pages/Login.jsx
+++ intro/src/pages/Login.jsx
@@ -4,17 +4,20 @@ import Signinform from "../components/Signinform";
 const Login = () => {
   return (
     <>
-      <div className="flex items-center">
-        <div className="hidden lg:flex p-3 h-screen bg-blue-900 w-1/2">
+      <div className="flex items-center bg-gray-100">
+        <div className="hidden lg:flex p-3 h-screen w-1/2">
           <h1 className="text-5xl">Login Page</h1>
         </div>
-        <main className="min-h-screen bg-gray-100 flex items-center justify-center w-full px-5 lg:px-8 lg:w-1/2">
+        <main className="min-h-screen flex items-center justify-center w-full px-5 lg:px-8 lg:w-1/2">
           <div className="w-full sm:max-w-md sm:mx-auto sm:w-full">
             <div className="bg-white py-8 px-5 rounded-lg shadow-lg lg:px-10">
               <Signinform />
               <div className="mt-4">
                 <p className="">
-                  Don't have an account? <Link to="" className="text-blue-500">Sign Up</Link>
+                  Don't have an account?{" "}
+                  <Link to="" className="text-blue-500">
+                    Sign Up
+                  </Link>
                 </p>
               </div>
             </div>
