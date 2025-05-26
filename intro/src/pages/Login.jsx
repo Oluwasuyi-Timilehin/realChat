1d2bad473efebe92a0e9b5091ec0c1c6e7954936
Oluwasuyi-Timilehin <oluwasuyitimilehin@gmail.com>
2025-05-26 23:25:04 +0100

    made changes

--- intro/src/pages/Login.jsx
+++ intro/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     <>
       <div className="flex items-center">
         <div className="hidden lg:flex p-3 h-screen bg-blue-900 w-1/2">
-          <h1 className="text-5xl">Login</h1>
+          <h1 className="text-5xl">Login Page</h1>
         </div>
         <main className="min-h-screen bg-gray-100 flex items-center justify-center w-full px-5 lg:px-8 lg:w-1/2">
           <div className="w-full sm:max-w-md sm:mx-auto sm:w-full">
