3a3db696c25904d9d1f11e94a02b3cf9da17d491
Oluwasuyi-Timilehin <oluwasuyitimilehin@gmail.com>
2025-05-27 01:48:32 +0100

    enhance Signinform with icons and add "Remember Me" option; update Login page layout

--- intro/src/components/Signinform.jsx
+++ intro/src/components/Signinform.jsx
@@ -1,3 +1,6 @@
+import { LuLock, LuMail } from "react-icons/lu";
+import { Link } from "react-router-dom";
+
 const Signinform = () => {
   return (
     <>
@@ -9,10 +12,15 @@ const Signinform = () => {
           >
             Email address
           </label>
-          <input
-            type="text"
-            className="mt-1 w-full py-2 px-3 border border-gray-300 rounded focus:outline-none"
-          />
+          <div className="mt-1 relative rounded shadow-sm">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <LuMail className="text-gray-400 h-5 w-5" />
+            </div>
+            <input
+              type="text"
+              className="mt-1 w-full py-2 pl-10 pr-5 border border-gray-300 rounded focus:outline-none"
+            />
+          </div>
         </div>
         <div className="">
           <label
@@ -21,12 +29,31 @@ const Signinform = () => {
           >
             Password
           </label>
-          <input
-            type="text"
-            className="mt-1 w-full py-2 px-3 border border-gray-300 rounded focus:outline-none"
-          />
+          <div className="mt-1 relative rounded shadow-sm">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <LuLock className="text-gray-400 h-5 w-5" />
+            </div>
+            <input
+              type="text"
+              className="mt-1 w-full py-2 pl-10 pr-5 border border-gray-300 rounded focus:outline-none"
+            />
+          </div>
         </div>
-        <button className="bg-emerald-600 w-full text-white rounded py-2 font-medium cursor-pointer">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2">
+            <input type="checkbox" className="cursor-pointer" />
+            <label htmlFor="checkbox" className="text-sm text-gray-700">
+              Remember Me
+            </label>
+          </div>
+          <Link
+            to="/forgotpassword"
+            className="underline text-sm text-red-600 cursor-pointer"
+          >
+            Forgot password?
+          </Link>
+        </div>
+        <button className="bg-emerald-600 w-full text-white rounded-md shadow-sm text-sm py-3 font-medium cursor-pointer">
           Log In
         </button>
       </form>
--- intro/src/pages/Login.jsx
+++ intro/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import Signinform from "../components/Signinform";
 
 const Login = () => {
@@ -10,12 +10,15 @@ const Login = () => {
         </div>
         <main className="min-h-screen flex items-center justify-center w-full px-5 lg:px-8 lg:w-1/2">
           <div className="w-full sm:max-w-md sm:mx-auto sm:w-full">
+            <h2 className="font-bold text-zinc-800 text-center text-2xl mb-7 lg:text-3xl">
+              Log in to your account
+            </h2>
             <div className="bg-white py-8 px-5 rounded-lg shadow-lg lg:px-10">
               <Signinform />
-              <div className="mt-4">
-                <p className="">
+              <div className="mt-4 text-center text-sm">
+                <p className="text-gray-700">
                   Don't have an account?{" "}
-                  <Link to="" className="text-blue-500">
+                  <Link to="" className="text-emerald-600 hover:text-emerald-500">
                     Sign Up
                   </Link>
                 </p>
